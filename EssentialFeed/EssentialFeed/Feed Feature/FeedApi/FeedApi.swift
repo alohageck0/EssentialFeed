dddc74737e658306ba6da3364d107d25cd532671
Evgenii <royalfiish@gmail.com>
2024-12-29 13:11:27 -0800

    refactor with FeedItemMapper

--- EssentialFeed/EssentialFeed/Feed Feature/FeedApi/FeedApi.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedApi/FeedApi.swift
@@ -39,8 +39,8 @@ public struct RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map { $0.item }))
+                if let items = try? FeedItemMapper.map(data, response) {
+                    completion(.success(items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -50,18 +50,27 @@ public struct RemoteFeedLoader {
         }
     }
     
-    struct Root: Decodable {
-        public let items: [Item]
-    }
-    
-    struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
+    struct FeedItemMapper {
+        struct Root: Decodable {
+            public let items: [Item]
+        }
         
-        var item: FeedItem {
-            FeedItem(id: id, description: description, location: location, imageURL: image)
+        struct Item: Decodable {
+            let id: UUID
+            let description: String?
+            let location: String?
+            let image: URL
+            
+            var item: FeedItem {
+                FeedItem(id: id, description: description, location: location, imageURL: image)
+            }
+        }
+        
+        static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+            guard response.statusCode == 200 else {
+                throw Error.invalidData
+            }
+            return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
         }
     }
 }
