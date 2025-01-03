dd7289de49d0acb2f477e7ee65101774f0d42aad
Evgenii <royalfiish@gmail.com>
2025-01-02 20:55:48 -0800

    Remove LoadFeedResult comformance to Equatable

--- EssentialFeed/EssentialFeed/FeedApi/LoadFeedResult.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LoadFeedResult.swift
-//  EssentialFeed
-//
-//  Created by Evgenii Iavorovich on 1/2/25.
-//
-
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -134,17 +134,24 @@ class RemoteFeedLoaderTests: XCTestCase {
         return try! JSONSerialization.data(withJSONObject: json)
     }
     
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        var capturedResults = [RemoteFeedLoader.Result]()
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         
-        sut.load() {
-            capturedResults.append($0)
-            debugPrint("evv actual \($0)" )
+        let exp = expectation(description: "Wait for load complition")
+        sut.load() { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedResult), .success(expectedResult)):
+                XCTAssertEqual(receivedResult, expectedResult, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected \(expectedResult) but got \(receivedResult)", file: file, line: line)
+            }
+        
+            exp.fulfill()
         }
         
         action()
-        
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class HTTPClientSpy: HTTPClient {
