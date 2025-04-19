948e18d73943beb597aeff6faf5b4a488577f627
Evgenii <royalfiish@gmail.com>
2025-04-19 10:15:47 -0700

    MVVM 1
    (cherry picked from commit 0450a27e82926706981c798848960eaff3b05d63)

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -8,8 +8,56 @@
 import UIKit
 import EssentialFeed
 
-public final class FeedRefreshViewController: NSObject {
+public final class FeedViewModel {
     private let loader: FeedLoader
+
+    init(loader: FeedLoader) {
+        self.loader = loader
+    }
+
+    enum State {
+        case pending
+        case loading
+        case loaded([FeedImage])
+        case failed
+    }
+
+    private var state = State.pending {
+        didSet {
+            onChange?(self)
+        }
+    }
+
+    var onChange: ((FeedViewModel) -> Void)?
+
+    var isLoading: Bool {
+        switch state {
+        case .loading: true
+        case .pending, .loaded, .failed: false
+        }
+    }
+
+    var feed: [FeedImage]? {
+        switch state {
+        case .loaded(let feed): feed
+        case .pending, .loading, .failed: nil
+        }
+    }
+
+    func loadFeed() {
+        state = .loading
+        loader.load() { [weak self] result in
+            if let feed = try? result.get() {
+                self?.state = .loaded(feed)
+            } else {
+                self?.state = .failed
+            }
+        }
+    }
+}
+
+public final class FeedRefreshViewController: NSObject {
+    private let viewModel: FeedViewModel
     public lazy var view: UIRefreshControl = {
         let view = UIRefreshControl()
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
@@ -17,19 +65,23 @@ public final class FeedRefreshViewController: NSObject {
     }()
     
     init(loader: FeedLoader) {
-        self.loader = loader
+        self.viewModel = FeedViewModel(loader: loader)
     }
     
     var onRefresh: (([FeedImage]) -> Void)?
     
     @objc func refresh() {
-        view.beginRefreshing()
-        
-        loader.load() { [weak self] result in
-            if let feed = try? result.get() {
+        viewModel.onChange = { [weak self] vm in
+            if vm.isLoading {
+                self?.view.beginRefreshing()
+            } else {
+                self?.view.endRefreshing()
+            }
+
+            if let feed = vm.feed {
                 self?.onRefresh?(feed)
             }
-            self?.view.endRefreshing()
         }
+        viewModel.loadFeed()
     }
 }
