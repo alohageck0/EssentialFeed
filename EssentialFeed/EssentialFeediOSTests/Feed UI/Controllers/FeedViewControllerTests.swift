70c4b94efc1010a337fa227317de29294775f2f9
Evgenii <royalfiish@gmail.com>
2025-05-06 14:30:09 -0700

    Extract FeedRefreshViewController test helpers to a separate file

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -368,33 +368,3 @@ private extension FeedImageCell {
         feedImageRetryButton.simulateTap()
     }
 }
-
-extension FeedRefreshViewController {
-    func replaceRefreshControlWithFakeForiOS17Support() {
-        let fake = FakeRefreshControl()
-        
-        view.allTargets.forEach { target in
-            view.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
-                fake.addTarget(target, action: Selector(action), for: .valueChanged)
-            }
-        }
-        
-        view = fake
-    }
-    
-    class FakeRefreshControl: UIRefreshControl {
-        private var _isRefreshing = false
-        
-        override var isRefreshing: Bool {
-            _isRefreshing
-        }
-        
-        override func beginRefreshing() {
-            _isRefreshing = true
-        }
-        
-        override func endRefreshing() {
-            _isRefreshing = false
-        }
-    }
-}
