29b6d055468f5e5d93c5de2a8e0a22e103c56e14
Evgenii <royalfiish@gmail.com>
2025-05-16 12:19:16 -0700

    Extract duplicate HTTPClientSpy helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedImageDataLoaderTests.swift
@@ -207,37 +207,4 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, client)
     }
-    
-    private class HTTPClientSpy: HTTPClient {
-        private struct Task: HTTPClientTask {
-            let callback: () -> Void
-            func cancel() { callback() }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task { [weak self] in
-                self?.cancelledURLs.append(url)
-            }
-        }
-        
-        var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        private(set) var cancelledURLs = [URL]()
-        
-        var requestedURLs: [URL] {
-            messages.map { $0.url}
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index],
-                                           statusCode: code,
-                                           httpVersion: nil,
-                                           headerFields: nil)!
-            messages[index].completion(.success((data, response)))
-        }
-    }
 }
--- EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedLoaderTests.swift
@@ -158,37 +158,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    private class HTTPClientSpy: HTTPClient {
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-        var requestedURL: URL?
-        
-        var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        
-        var requestedURLs: [URL] {
-            messages.map { $0.url}
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            requestedURL = url
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index],
-                                           statusCode: code,
-                                           httpVersion: nil,
-                                           headerFields: nil)!
-            messages[index].completion(.success((data, response)))
-        }
-    }
-    
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (HTTPClientSpy, RemoteFeedLoader) {
         let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
